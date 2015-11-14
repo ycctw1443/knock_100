e77f10b3c4a1798b8bc710697ee18b4d4a4f042b
ycctw1443 <ycctw1443@gmail.com>
2015-11-15 00:13:31 +0900

    問11の作成

---
+++ k11.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-- coding:utf-8 -*-
+import codecs
+import re
+
+def main():
+    with codecs.open('hightemp.txt','r','utf-8') as fin:
+        print(''.join([re.sub(r'\t',' ',x) for x in fin]))
+
+if __name__ == '__main__':
+    main()

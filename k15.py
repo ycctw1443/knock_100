12660c5f39812eb860796de29b7c4a4d38aac9d1
ycctw1443 <ycctw1443@gmail.com>
2015-11-15 00:52:12 +0900

    問15の作成

---
+++ k15.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-- coding:utf-8 -*-
+import codecs
+
+def main():
+    num = int(input())
+    with codecs.open('hightemp.txt','r','utf-8') as fin:
+        list = [line for line in fin]
+        print(''.join(list[len(list)-num:]))
+
+if __name__ == '__main__':
+    main()

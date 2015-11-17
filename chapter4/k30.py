fa9565acdc6b7cecf1e6e7756d104c2f62a11ae8
ycctw1443 <ycctw1443@gmail.com>
2015-11-18 03:04:13 +0900

    問30の作成

---
+++ chapter4/k30.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#-- coding:utf-8 -*-
+import codecs
+import re
+try:
+    import cPickle as pickle
+except:
+    import pickle
+#今回、作られたリストはpickleファイルとして保存します。
+
+def get_mecab(path):
+    list = []
+    output_list = []
+    with codecs.open(path,'r','utf-8') as fin:
+        for line in fin:
+            if line == 'EOS\n' and len(list) >= 1:
+                output_list.append(list)
+            elif line != '':
+                word = line.split(',')
+                list.append({'surface':word[0].split('\t')[0], 'base':word[6], 'pos':word[0].split('\t')[1], 'pos1':word[1]})
+    return output_list
+
+def main():
+    list = get_mecab('neko.txt.mecab')
+    pickle.dump(list,open('neko.txt.mecab.pkl','wb'))
+
+if __name__ == '__main__':
+    main()
